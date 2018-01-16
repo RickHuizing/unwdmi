4670e3105ab14638f3dae577c032c87429cbfad4
Rick Huizing <rickhuizing@live.nl>
2018-01-16 13:50:02 +0100

    gaat connectie aan met 1 cluster en print en telt ontvanen pakketjes

---
+++ src/Runner.java
@@ -0,0 +1,10 @@
+public class Runner{
+    public static void main(String[] args) throws Exception {
+        //sqltest dao = new sqltest();
+        //dao.readDataBase();
+        receiver rec = new receiver();
+        rec.connect();
+
+    }
+
+}
